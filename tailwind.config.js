e0d1abd89e5ee248efb86423a8d71dd2b3e58806
farhana <farhana.capricon@gmail.com>
2025-05-23 11:54:40 +0200

    Basic Layout of my profile

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",  // Include all HTML/JS files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
