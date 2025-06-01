fe08360f7fb78881e1a33ac5e8421306d44fc8c3
farhana <farhana.capricon@gmail.com>
2025-06-01 13:21:59 +0200

    Add project files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./*.html",
     "./src/**/*.{html,js}",  // Include all HTML/JS files in src
   ],
   theme: {
